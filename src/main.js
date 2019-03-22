faa699830d5f9b61890ecd1abdc693e689fbe331
ighigoo <ighigoo@gmail.com>
2019-03-22 19:17:54 +0800

    dir reset

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import axios from 'axios'  // ajax的套件
+import VueAxios from 'vue-axios' // 轉為Vue的套件
 import App from './App'
 import router from './router'
 
 Vue.config.productionTip = false
+Vue.use(VueAxios, axios)
 
 /* eslint-disable no-new */
 new Vue({
