fdffeb0090e63c7fde2554231a4b5de055c8e141
ighigoo <ighigoo@gmail.com>
2019-03-22 20:27:10 +0800

    bootstrap & vue-axios setting

--- src/App.vue
+++ src/App.vue
@@ -7,17 +7,17 @@
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App",
+  created() {
+    // api test
+    const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/products`;
+    this.$http.get(api).then(response => {
+      console.log(response.data);
+    });
+  }
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+@import "./assets/all";
 </style>
